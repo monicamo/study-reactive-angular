9edf66faad5a6c32fc212b9305db8ef1fa2a56b8
Monica Monteiro <monica.monteiro@estadao.com>
2024-06-05 10:33:32 -0300

    Setup inicial

---
+++ src/app/course-dialog/course-dialog.component.ts
@@ -0,0 +1,55 @@
+import {AfterViewInit, Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from "@angular/material/dialog";
+import {Course} from "../model/course";
+import {FormBuilder, Validators, FormGroup} from "@angular/forms";
+import moment from 'moment';
+import { CoursesService } from '../services/courses.service';
+
+@Component({
+    selector: 'course-dialog',
+    templateUrl: './course-dialog.component.html',
+    styleUrls: ['./course-dialog.component.css']
+})
+export class CourseDialogComponent implements AfterViewInit {
+
+    form: FormGroup;
+
+    course:Course;
+
+    constructor(
+        private fb: FormBuilder,
+        private coursesService: CoursesService,
+        private dialogRef: MatDialogRef<CourseDialogComponent>,
+        @Inject(MAT_DIALOG_DATA) course:Course) {
+
+        this.course = course;
+
+        this.form = fb.group({
+            description: [course.description, Validators.required],
+            category: [course.category, Validators.required],
+            releasedAt: [moment(), Validators.required],
+            longDescription: [course.longDescription,Validators.required]
+        });
+
+    }
+
+    ngAfterViewInit() {
+
+    }
+
+    save() {
+
+      const changes = this.form.value;
+      this.coursesService.saveCourse(this.course.id, changes)
+        .subscribe(
+          val => {
+            this.dialogRef.close(val);
+          }
+        );
+    }
+
+    close() {
+        this.dialogRef.close();
+    }
+
+}
